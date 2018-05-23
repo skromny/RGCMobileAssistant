5f625b9bec91eded5f89f3dab986faa423f3d023
Skromny <artur.nowak@live.com>
2018-05-24 00:10:12 +0200

    proj fix

---
+++ mobile/src/main/java/com/recrutify/rgc/mobileassistant/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.recrutify.rgc.mobileassistant
+
+import android.arch.lifecycle.ViewModel
+import javax.inject.Inject
+
+class MainViewModel @Inject constructor() : ViewModel() {
+
+    val name = "MainViewModel.ok"
+}
